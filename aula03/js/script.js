3e0032b9576a10f302fbcd700633b983fd2a9619
caique lancioni <caiquelanicioni27@gmail.com>
2024-04-20 11:58:46 -0300

    aula 3 if else e for

---
+++ aula03/js/script.js
@@ -0,0 +1,8 @@
+numero = prompt("ditite um numero")
+// vamos calcular o numero digitado em busca do
+if(numero % 2 == 0){
+    alert("o numero digitado é par");
+}
+else{
+    alert("o numero digitado é impar");
+}
---
+++ aula03/js/script3.js
@@ -0,0 +1,14 @@
+function verifica(){
+    var nota = document.getElementsByTagName("input")[0].value;
+    var resultado="aprovado"
+    if(nota >=7){
+        resultado
+    }
+    else if(nota <=4){
+        resultado = "reprovado"
+    }
+    else{
+        resultado = "reculperação"
+    }
+    document.getElementsByTagName("label")[1].innerHTML=resultado
+}
